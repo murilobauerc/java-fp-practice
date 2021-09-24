89e651d06826a54ece98beaab0dc46bca48ed072
murilobauerc <murilobauerc@gmail.com>
2021-09-24 20:48:27 -0300

    finding only true values with filter

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -1,19 +1,28 @@
 package programming;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FP01Functional {
 
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(50,1,1,2,40,203,40291,202));
+        printAllNumbersInListFunctional(List.of(true,false,true,false,false,true,true));
     }
 
     private static void print(int number) {
         System.out.println(number);
     }
 
-    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-        // Convert the list of numbers in a stream of numbers (a sequence of numbers, first 50, then 1...)
-        numbers.stream().forEach(FP01Functional::print); // method reference
+    private static void printAllNumbersInListFunctional(List<Boolean> numbers) {
+        // Find the true values
+        // Functional
+       numbers.stream().filter(item -> item.equals(true)).forEach(System.out::println);
+
+       // Estruturado
+        for(int i = 0; i < numbers.size(); i++) {
+            if(numbers.get(i)) {
+                System.out.println(numbers.get(i));
+            }
+        }
     }
 }
--- src/programming/FP01Structured.java
+++ src/programming/FP01Structured.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class FP01Structured {
 
     public static void main(String[] args) {
-	    printAllNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
-    }
-
-    private static void printAllNumbersInListStructured(List<Integer> numbers) {
-        for(int number : numbers) {
-            System.out.println("Number: " + number);
+        // Estruturada
+        final List<Integer> integers = List.of(1, 2, 5, 1013, 3, 3, 23, 213);
+        for(Integer number : integers) {
+            System.out.println(number);
         }
     }
 }
